205848ae7993df6b4f866b054081f18b42e2d57f
ananyavatsa27 <ananya.vatsa_cs24@gla.ac.in>
2025-08-21 23:45:19 +0530

    HarryAge.java Addition

---
+++ Level 1/HarryAge.java
@@ -0,0 +1,8 @@
+class HarryAge {
+    public static void main(String[] args) {
+        int currentYear = 2024;
+        int birthYear = 2000;
+        int age = currentYear - birthYear;
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
