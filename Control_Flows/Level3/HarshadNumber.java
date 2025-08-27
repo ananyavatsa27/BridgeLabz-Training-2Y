29280c9f349e369ab687ba9853d34b2843afa305
ananyavatsa27 <ananya.vatsa_cs24@gla.ac.in>
2025-08-27 21:50:12 +0530

    file added

---
+++ Control_Flows/Level3/HarshadNumber.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        
+        int sumOfDigits = 0;
+        int temp = number;
+
+        while (temp > 0) {
+            sumOfDigits += temp % 10;
+            temp /= 10;
+        }
+
+        if (number % sumOfDigits == 0) {
+            System.out.println(number + " is a Harshad Number.");
+        } else {
+            System.out.println(number + " is NOT a Harshad Number.");
+        }
+
+        sc.close();
+    }
+}
